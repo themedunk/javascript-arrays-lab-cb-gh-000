8a98a1bc09ac288f37837255b03b23e020ff8a80
Nguyen Duc Thuan <foxgift.net@gmail.com>
2018-06-17 18:30:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const app = "I don't do much."
-<<<<<<< HEAD
 function destructivelyAppendKitten(name) {
-  kittens.pops(name);
+  return kittens.push(name);
 }
-=======
-sádsad
->>>>>>> e4213c103661c482d856d9374c3ef91d0e04fafa
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name);
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop();
+}
+function destructivelyRemoveFirstKitten() {
+  return kitten.shift();
+}
+function appendKitten(name) {
+  var cats = kittens;
+  cats.push(name)
+  return cats;
+}
+function removeLastKitten() {
+  var cats = kittens;
+  cats.pop();
+  return cats;
+}
+function removeFirstKitten() {
+  var cats = kittens;
+  cats.shift();
+  return cats;
+}
